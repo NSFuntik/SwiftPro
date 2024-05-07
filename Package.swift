178fa4cffb597fa0311144d47cc65c8d119a2ccb
Дмитрий Михайлов <nsfuntik@gmail.com>
2024-05-07 13:54:00 +0300

    D

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
         .package(url:"https://github.com/apple/swift-collections.git",
                  .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/shaps80/SwiftUIBackports", from: "2.8.0"),
+        .package(url: "https://github.com/hmlongco/Factory.git", from: "2.3.2"),
+
         .package(url: "https://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@github.com/NSFuntik/SFSymbolEnum", branch: "main")
         
     ],
@@ -28,11 +30,12 @@ let package = Package(
             name: "SwiftPro",
             dependencies: [
                 "SwiftUIBackports",
+                .product(name: "Factory", package: "factory"),
                 .product(name: "OrderedCollections", package: "swift-collections"),
                 .product(name: "SFSymbolEnum", package: "SFSymbolEnum")
             ],
             resources: [
-                
+                .process("Resources/Media.xcassets"),
             ])
     ]
 )
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![ios](https://img.shields.io/badge/iOS-0C62C7)
 [![swift](https://img.shields.io/endpoint?url=https%3A%2F%2Fswiftpackageindex.com%2Fapi%2Fpackages%2Fshaps80%2FSwiftPro%2Fbadge%3Ftype%3Dswift-versions)](https://swiftpackageindex.com/shaps80/SwiftPro)
 
-# SwiftUI Plus
+# SwiftPro
 
 Introducing a collection of SwiftUI additions to make your projects easier to build.
 
