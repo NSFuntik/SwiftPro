61b619e7b7838ce54d1f1224fc8681f93b3a7472
Дмитрий Михайлов <nsfuntik@gmail.com>
2024-05-17 16:15:56 +0300

    v upd

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftPro",
     platforms: [
-        .iOS(.v15), .macOS(.v13), .tvOS(.v15), .watchOS(.v8), .macCatalyst(.v15)
+        .iOS(.v15), .macOS(.v14), .tvOS(.v15), .watchOS(.v8), .macCatalyst(.v15)
     ],
     products: [
         .library(
